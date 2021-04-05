0e4e658dc9cbe95860d7149c0245c6f167da9f2d
alperen-kilic <alp.kilic.97@gmail.com>
2021-04-05 09:44:23 +0300

    'auth.php' dosyasındaki typo düzeltildi.

--- tr/auth.php
+++ tr/auth.php
@@ -14,5 +14,5 @@ return [
     */
 
     'failed' => 'Girilmiş olan kullanıcı verileri sistemdekiler ile eşleşmemektedir.',
-    'throttle' => 'Çok fazla oturum açma girişiminde bulundunuz. Lütfen :seconds saniye sonra tekrar deneyiz.',
+    'throttle' => 'Çok fazla oturum açma girişiminde bulundunuz. Lütfen :seconds saniye sonra tekrar deneyiniz.',
 ];
